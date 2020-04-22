9d925fdd6e12befc1092364bf8d0b94e6573e222
Amandus Søve Thorsrud <ama.thorsrud@gmail.com>
2020-04-22 19:22:08 +0200

    Remove OpenGL support

--- src/lib.rs
+++ src/lib.rs
@@ -18,31 +18,10 @@ pub fn run() {
 }
 
 fn create_window(event_loop: &EventLoop<()>, size: (u32, u32)) -> winit::window::Window {
-    #[cfg(not(feature = "gl"))]
-    let window = {
-        winit::window::WindowBuilder::new()
-            .with_inner_size(winit::dpi::PhysicalSize::<u32>::from(size))
-            .with_title("Mandelbrot Visualization")
-            .with_resizable(false)
-            .build(&event_loop)
-            .unwrap()
-    };
-
-    #[cfg(feature = "gl")]
-    let (window, instance) = {
-        let wb = winit::window::WindowBuilder::new()
-            .with_inner_size(winit::dpi::PhysicalSize::<u32>::from(state.window_size))
-            .with_title("Mandelbrot Visualization")
-            .with_resizable(false);
-        let cb = wgpu::glutin::ContextBuilder::new().with_vsync(true);
-        let context = cb.build_windowed(wb, &event_loop).unwrap();
-
-        let (context, window) = unsafe { context.make_current().unwrap().split() };
-
-        let instance = wgpu::Instance::new(context);
-
-        (window, instance)
-    };
-
-    window
+    winit::window::WindowBuilder::new()
+        .with_inner_size(winit::dpi::PhysicalSize::<u32>::from(size))
+        .with_title("Mandelbrot Visualization")
+        .with_resizable(false)
+        .build(&event_loop)
+        .unwrap()
 }
